604676c6d07f6dce0fd36621d55fd9e2379950d2
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2024-08-30 11:36:38 +0200

    Code cleanup and minor changes
    Created script night_mode.js
    Changed padding from 2em to 1em

---
+++ public/scripts/night_mode.js
@@ -0,0 +1,21 @@
+(function() {
+    updateNightModeButtonText();
+})();
+
+document.addEventListener('DOMContentLoaded', updateNightModeButtonText);
+
+function nightModeButtonClick() {
+    toggleTheme();
+    updateNightModeButtonText();
+}
+
+function updateNightModeButtonText() {
+    const theme = getCookie('data-theme');
+    const button = document.getElementById('themeToggleBtn');
+    button.textContent = theme === 'dark' ? 'Deactivate night mode' : 'Activate night mode';
+}
+
+function toggleTheme() {
+    const theme = getCookie('data-theme') === 'dark' ? 'light' : 'dark';
+    setTheme(theme);
+}
