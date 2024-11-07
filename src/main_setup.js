8885a2a3b0b3279a4db92f9d1cc71d9136a6d8e3
Thomas Sainrat <thomas.sainrat@ligo.org>
2024-11-07 16:14:42 +0100

    Use glob for files instead of hardcoded names

--- src/main_setup.js
+++ src/main_setup.js
@@ -3,6 +3,7 @@ const ejs = require("ejs");
 const path = require("path");
 
 const parser = require("./parser.js");
+const { globSync } = require('glob');
 
 let port = process.env.PORT || 3000;
 if (!isNaN(parseInt(process.argv[2]))) {
@@ -31,63 +32,6 @@ const stats_variables = [
 ];
 
 /// ---
-
-const chapters = [
-    "ch1",
-    "ch2",
-    "ch3",
-    "ch4",
-    "ch5",
-    "ch6",
-    "ch7",
-    "ch8",
-    "ch9",
-    "ch10",
-    "ch11a",
-    "ch11b",
-    "b2ch1",
-    "b2ch2",
-    "b2ch3",
-    "b2ch4a",
-    "b2ch4b",
-    "b2ch5a",
-    "b2ch5b",
-    "b2ch6",
-    "b2ch7",
-    "b2ch8",
-    "b2ch9a",
-    "b2ch9b",
-    "b2ch10a",
-    "b2ch10b",
-    "b2ch11a",
-    "b2ch11b",
-    "b2ch11c",
-    "b3ch1",
-    "b3ch2a",
-    "b3ch2b",
-    "b3ch2c",
-    "b3ch3a",
-    "b3ch3b",
-    "b3ch4a",
-    "b3ch4b",
-    "b3ch5a",
-    "b3ch5b",
-    "b3ch6a",
-    "b3ch6b",
-    "b3ch6c",
-    "b3ch7a",
-    "b3ch8a",
-    "b3ch8b",
-    "b3ch9a",
-    "b3ch9b",
-    "b3ch9c",
-    "b3ch10a",
-    "b3ch10b",
-    "b3ch10c",
-    "b3ch11a",
-    "b3ch12a",
-    "b3ch12b",
-];
 
 function apply_condition(entry, values) {
     if (!entry) {
@@ -415,9 +359,10 @@ let book;
 
 Object.keys(locales).forEach( function(locale) {
     magiumData[locale] = {}
-    for (chapter of chapters) {
+    const chapterFiles = globSync(path.join(dirname,`data/${locale}/*.magium`))
+    for (chapterFile of chapterFiles) {
         parser
-            .parse(path.join(dirname, `data/${locale}/${chapter}.magium`))
+            .parse(chapterFile)
             .then((val) => Object.assign(magiumData[locale], val));
     }
 
