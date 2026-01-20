e43c48757ca97e6894a89f898539b3cecfba207f
thuiop <thuiop@hotmail.fr>
2026-01-20 11:47:59 +0100

    Fix export

--- public/scripts/utils.js
+++ public/scripts/utils.js
@@ -48,4 +48,30 @@ function setResponseVariables(response) {
       storeVariable(key,value)
   }
 }
+async function copyToClipboard(textToCopy) {
+    // Navigator clipboard api needs a secure context (https)
+    if (navigator.clipboard && window.isSecureContext) {
+        await navigator.clipboard.writeText(textToCopy);
+    } else {
+        // Use the 'out of viewport hidden text area' trick
+        const textArea = document.createElement("textarea");
+        textArea.value = textToCopy;
+            
+        // Move textarea out of the viewport so it's not visible
+        textArea.style.position = "absolute";
+        textArea.style.left = "-999999px";
+            
+        document.body.prepend(textArea);
+        textArea.select();
+
+        try {
+            document.execCommand('copy');
+        } catch (error) {
+            console.error(error);
+        } finally {
+            textArea.remove();
+        }
+    }
+}
+
 
